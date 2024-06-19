c1b87a28c337a6f96233bef0aefd8df3fdfb1ab5
SteffRhes <steffrhes@pm.me>
2024-06-19 11:44:52 +0100

    update modification

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM python:3.11.9
 RUN pip install notebook==7.0.6
-RUN pip install gensim
+RUN pip install gensim==4.3.2
 RUN pip install scipy==1.10.1
+RUN pip install matplotlib==3.9.0
+RUN pip install scikit-learn==1.5.0
+
--- src/train/train.py
+++ src/train/train.py
@@ -1,37 +1,17 @@
 import os
 import gensim
-from gensim.utils import simple_preprocess
 from gensim.models.word2vec import LineSentence
-from datetime import datetime
-import subprocess
-import yaml
 
 
-# train data
 TRAIN_DATA_PATH = "/veld/input/" + os.getenv("in_train_data_file")
-
-# model data
 MODEL_PATH = "/veld/output/" + os.getenv("out_model_file")
-
-# model hyperparameters
 EPOCHS = int(os.getenv("epochs"))
 VECTOR_SIZE = int(os.getenv("vector_size"))
 WINDOW = int(os.getenv("window"))
 MIN_COUNT = int(os.getenv("min_count"))
 
-
-print(f"TRAIN_DATA_PATH: {TRAIN_DATA_PATH}")
-print(f"MODEL_PATH: {MODEL_PATH}")
-print(f"VECTOR_SIZE: {VECTOR_SIZE}")
-print(f"WINDOW: {WINDOW}")
-print(f"MIN_COUNT: {MIN_COUNT}")
-
-
-sentences = LineSentence(TRAIN_DATA_PATH)
-
-
 model = gensim.models.Word2Vec(
-    sentences=sentences,
+    sentences=LineSentence(TRAIN_DATA_PATH),
     epochs=EPOCHS,
     vector_size=VECTOR_SIZE,
     window=WINDOW,
@@ -39,3 +19,4 @@ model = gensim.models.Word2Vec(
     workers=os.cpu_count(),
 )
 model.save(MODEL_PATH)
+
